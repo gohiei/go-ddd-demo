d55b60580661bad6664f1d62bffa157e9f836ab2
chuck <chuck@mail.chungyo.net>
2023-05-16 16:42:53 +0800

    [Feature]拆分 controller 為 service/controller

--- internal/user/adapter/user_service.go
+++ internal/user/adapter/user_service.go
@@ -2,23 +2,9 @@ package user
 
 import (
 	"cypt/internal/dddcore"
-	repo "cypt/internal/user/repository"
 	usecase "cypt/internal/user/usecase"
 	"fmt"
 )
-
-type UserController interface {
-	RegisterUser(username string, password string) (usecase.RegisterUserUseCaseOutput, error)
-	Rename(id string, username string) (usecase.RenameUseCaseOutput, error)
-}
-
-func NewController(r repo.UserRepository, eventBus dddcore.EventBus) UserController {
-	return &UserService{
-		RegisterUserUseCase:  usecase.NewRegisterUserUseCase(r, eventBus),
-		RenameUseCase:        usecase.NewRenameUseCase(r, eventBus),
-		NotifyManagerHandler: usecase.NewNotifyManagerHandler(eventBus),
-	}
-}
 
 type UserService struct {
 	RegisterUserUseCase  dddcore.UseCase[usecase.RegisterUserUseCaseInput, usecase.RegisterUserUseCaseOutput]
