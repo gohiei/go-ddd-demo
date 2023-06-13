096adbbb1c19c061bdd19644407bef3cc6db9264
chuck <chuck@mail.chungyo.net>
2023-06-13 10:27:20 +0800

    feat: Remove useless log.Println

--- internal/infra/user_db.go
+++ internal/infra/user_db.go
@@ -1,8 +1,6 @@
 package infra
 
 import (
-	"log"
-
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
@@ -10,15 +8,6 @@ import (
 )
 
 func NewUserDB(writeDsn string, readDsn string) (*gorm.DB, error) {
-	// writeDsn := os.Getenv("USER_WRITE_DB_DSN")
-	// readDsn := os.Getenv("USER_READ_DB_DSN")
-	// writeDsn := viper.GetString("USER_WRITE_DB_DSN")
-	// readDsn := viper.GetString("USER_READ_DB_DSN")
-
-	log.Println("new user db")
-	log.Println(writeDsn, readDsn)
-	log.Println("end new user db")
-
 	db, err := gorm.Open(mysql.Open(writeDsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Silent),
 	})
