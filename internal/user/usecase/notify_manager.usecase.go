f7a4ad641ed8aedbafe6359efc069f186e5e9729
chuck <chuck@mail.chungyo.net>
2023-07-18 16:54:02 +0800

    style: Rearrange the filename

---
+++ internal/user/usecase/notify_manager.usecase.go
@@ -0,0 +1,54 @@
+package usecase
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"cypt/internal/dddcore"
+	"cypt/internal/user/entity/events"
+	"cypt/internal/user/repository"
+)
+
+var _ dddcore.UseCase[NotifyManagerUseCaseInput, NotifyManagerUseCaseOutput] = (*NotifyManagerUseCase)(nil)
+
+// NotifyManagerUseCase is a handler for NotifyManager events.
+type NotifyManagerUseCase struct {
+	repo repository.OutsideRepository
+}
+
+func (h *NotifyManagerUseCase) Name() string {
+	return "user.notify.manager"
+}
+
+func (h *NotifyManagerUseCase) EventName() string {
+	return events.UserRenamedEventName
+}
+
+func (h *NotifyManagerUseCase) When(eventName string, msg []byte) {
+	event := events.UserRenamedEvent{}
+
+	if err := json.Unmarshal(msg, &event); err != nil {
+		return
+	}
+
+	_, _ = h.Execute(&NotifyManagerUseCaseInput{})
+}
+
+type NotifyManagerUseCaseInput struct{}
+type NotifyManagerUseCaseOutput bool
+
+func (h *NotifyManagerUseCase) Execute(input *NotifyManagerUseCaseInput) (NotifyManagerUseCaseOutput, error) {
+	if data, err := h.repo.GetEchoData(); err != nil {
+		// nolint: forbidigo
+		fmt.Println("Echo: ", data)
+	}
+
+	return false, nil
+}
+
+func NewNotifyManagerHandler(repo repository.OutsideRepository, eb dddcore.EventBus) NotifyManagerUseCase {
+	h := NotifyManagerUseCase{repo: repo}
+	eb.Register(&h)
+
+	return h
+}
