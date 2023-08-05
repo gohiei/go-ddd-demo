1c4c87e28b2ab53f80d7be7eeed94f91858e8d6c
chuck <chuck@mail.chungyo.net>
2023-08-05 20:46:22 +0800

    feat: Return error for all methods of `EventBus`

--- internal/dddcore/event_bus.go
+++ internal/dddcore/event_bus.go
@@ -2,10 +2,10 @@ package dddcore
 
 // EventBus represents an event bus that facilitates event publishing and subscription.
 type EventBus interface {
-	Post(ev Event)
-	PostAll(ar AggregateRoot)
-	Register(h EventHandler)
-	Unregister(h EventHandler)
+	Post(ev Event) error
+	PostAll(ar AggregateRoot) error
+	Register(h EventHandler) error
+	Unregister(h EventHandler) error
 }
 
 // EventHandler represents a handler for events.
--- test/mocks/dddcore/EventBus.go
+++ test/mocks/dddcore/EventBus.go
@@ -14,23 +14,59 @@ type EventBus struct {
 }
 
 // Post provides a mock function with given fields: ev
-func (_m *EventBus) Post(ev dddcore.Event) {
-	_m.Called(ev)
+func (_m *EventBus) Post(ev dddcore.Event) error {
+	ret := _m.Called(ev)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(dddcore.Event) error); ok {
+		r0 = rf(ev)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
 }
 
 // PostAll provides a mock function with given fields: ar
-func (_m *EventBus) PostAll(ar dddcore.AggregateRoot) {
-	_m.Called(ar)
+func (_m *EventBus) PostAll(ar dddcore.AggregateRoot) error {
+	ret := _m.Called(ar)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(dddcore.AggregateRoot) error); ok {
+		r0 = rf(ar)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
 }
 
 // Register provides a mock function with given fields: h
-func (_m *EventBus) Register(h dddcore.EventHandler) {
-	_m.Called(h)
+func (_m *EventBus) Register(h dddcore.EventHandler) error {
+	ret := _m.Called(h)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(dddcore.EventHandler) error); ok {
+		r0 = rf(h)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
 }
 
 // Unregister provides a mock function with given fields: h
-func (_m *EventBus) Unregister(h dddcore.EventHandler) {
-	_m.Called(h)
+func (_m *EventBus) Unregister(h dddcore.EventHandler) error {
+	ret := _m.Called(h)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(dddcore.EventHandler) error); ok {
+		r0 = rf(h)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
 }
 
 // NewEventBus creates a new instance of EventBus. It also registers a testing interface on the mock and a cleanup function to assert the mocks expectations.
